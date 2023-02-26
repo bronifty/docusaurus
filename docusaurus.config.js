510c5e8500f2ee30bb808e20fa5e692462ef17b5
bronifty <bronifty@gmail.com>
2023-02-26 17:37:24 -0500

    updated link to github

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -119,7 +119,7 @@ const config = {
               },
               {
                 label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
+                href: "https://github.com/bronifty",
               },
             ],
           },
